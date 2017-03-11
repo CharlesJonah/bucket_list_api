75aabd425bd32a9467d7a06b250a0a5b1f5ba852
CharlesJonah <mutua.charles48@gmail.com>
2017-03-11 16:18:57 +0300

    [Chore] Add more comments

---
+++ application/serializer.py
@@ -0,0 +1,23 @@
+'''
+This module maps the data that will be used by the marshall when returning the
+data to the user
+'''
+
+from flask_restful import fields
+
+bucket_list_item_serializer = {
+    'item_id': fields.Integer,
+    'name': fields.String,
+    'date_created': fields.DateTime,
+    'date_modified': fields.DateTime,
+    'done': fields.Boolean
+}
+
+bucket_list_serializer = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'items':fields.Nested(bucket_list_item_serializer),
+    'created_by': fields.String,
+    'date_created': fields.DateTime,
+    'date_modified': fields.DateTime
+}
