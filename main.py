c930d030a8c5a5d54487b03303771a2758febaf3
CharlesJonah <mutua.charles48@gmail.com>
2017-03-11 16:02:48 +0300

    [Chore] Add a resources endpoint

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask_restful import Api
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from application.models import db
+from application import models
+from application.views import UserRegistration, GetBucketLists, DeleteBucketList,\
+                              DeleteBucketListItem, CreateBucketListItems, GetBucketList,UpdateBucketList,\
+                              UpdateBucketListItem, UserLogin, CreateBucketList
+from application.config import Config
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = Config.SQLALCHEMY_DATABASE_URI
+api = Api(app)
+db.app = app
+db.init_app(app)
+db.create_all()
+
+api.add_resource(UserRegistration, '/v1/auth/register')
+api.add_resource(GetBucketLists, '/v1/bucketlists')
+api.add_resource(CreateBucketListItems, '/v1/bucketlists/<int:id>/items')
+api.add_resource(UpdateBucketListItem, '/v1/bucketlists/<int:id>/items/<int:item_id>')
+api.add_resource(DeleteBucketListItem, '/v1/bucketlists/<int:id>/items/<int:item_id>')
+api.add_resource(CreateBucketList, '/v1/bucketlists')
+api.add_resource(GetBucketList, '/v1/bucketlists/<int:id>')
+api.add_resource(UpdateBucketList, '/v1/bucketlists/<int:id>')
+api.add_resource(DeleteBucketList, '/v1/bucketlists/<int:id>')
+api.add_resource(UserLogin, '/v1/auth/login')
+
+if __name__ == '__main__':
+   app.run(debug = True, host='127.0.0.1', port=5200)
