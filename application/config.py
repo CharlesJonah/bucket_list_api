8cbecb821c5f7954313bcbd733a7424bd0da15c5
CharlesJonah <mutua.charles48@gmail.com>
2017-03-11 17:25:11 +0300

    [Chore] Changed database configuration

--- application/config.py
+++ application/config.py
@@ -1,15 +1,16 @@
 import os
+import dj_database_url
 
 class Config(object):
     """ This class configures the parameters to be used in a production enviroment"""
     CSRF_ENABLED = True
     SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
+    SQLALCHEMY_DATABASE_URI = dj_database_url.config()
 
 class Test(object):
     """ This class configures the parameters to be used in a test enviroment"""
     CSRF_ENABLED = True
-    SQLALCHEMY_TEST_DATABASE_URI = os.environ["SQLALCHEMY_TEST_DATABASE_URI"]
+    SQLALCHEMY_TEST_DATABASE_URI = dj_database_url.config()
     SECRET_KEY = os.environ["SECRET_KEY"]
 
 
