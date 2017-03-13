7128fc6ec7273c27ff2212c409b81965d014ba72
CharlesJonah <mutua.charles48@gmail.com>
2017-03-13 10:31:24 +0300

    [Bug] Fixes database connection error

--- application/config.py
+++ application/config.py
@@ -5,12 +5,12 @@ class Config(object):
     """ This class configures the parameters to be used in a production enviroment"""
     CSRF_ENABLED = True
     SECRET_KEY = os.environ["SECRET_KEY"]
-    SQLALCHEMY_DATABASE_URI = dj_database_url.config()
+    SQLALCHEMY_DATABASE_URI = os.environ["SQLALCHEMY_DATABASE_URI"]
 
 class Test(object):
     """ This class configures the parameters to be used in a test enviroment"""
     CSRF_ENABLED = True
-    SQLALCHEMY_TEST_DATABASE_URI = dj_database_url.config()
+    SQLALCHEMY_TEST_DATABASE_URI = 'sqlite:///' + 'test_database'
     SECRET_KEY = os.environ["SECRET_KEY"]
 
 
