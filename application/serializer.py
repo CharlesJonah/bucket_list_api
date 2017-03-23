fda10173d0970a24c4ab2f3085dd9d340ff0a0db
CharlesJonah <mutua.charles48@gmail.com>
2017-03-23 12:07:43 +0300

    [Chore] Fixes wrong named field

--- application/serializer.py
+++ application/serializer.py
@@ -6,7 +6,7 @@ data to the user from the database
 from flask_restful import fields
 
 bucket_list_item_serializer = {
-    'item_id': fields.Integer,
+    'id': fields.Integer,
     'name': fields.String,
     'date_created': fields.DateTime,
     'date_modified': fields.DateTime,
